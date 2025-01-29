3e4d212ca2ff99b99fb56e9d10b2d1981b08b4bf
Zaki <mahmedzaki670@gmail.com>
2025-01-29 22:02:14 +0500

    vercel.json file

--- myproject/main/asgi.py
+++ myproject/main/asgi.py
@@ -18,5 +18,5 @@ application = get_asgi_application()
 import os
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "your_project.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
 application = get_asgi_application()
